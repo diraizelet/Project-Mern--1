8d71198cc1875c79c528313686a844acf3d8e84b
diraizelet <varunsuren2003@gmail.com>
2024-03-03 12:43:16 +0530

    parent to child communication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import "./App.css";
-import GoalList from './components/GoalList';
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
+
 const App = () => {
+  const courseGoals = [
+    {id: 'cg1', text: 'Finish a full blown project'},
+    {id: 'cg2', text: 'Deploy the project to view from anywhere'},
+    {id: 'cg3', text: 'Be firm in all the possible and required knowledges about MERN completely'}
+  ];
+
   return (<div className="CourseGoals">
     <h2>My Goals</h2>
-    <GoalList/>
+    <NewGoal />
+    <GoalList goals={courseGoals}/>
   </div>);
+
 };
 
 export default App;
